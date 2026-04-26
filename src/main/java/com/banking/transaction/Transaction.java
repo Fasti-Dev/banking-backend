6538fcaadbf31702dfba9d51d0e747a2946d046a
Fasti-Dev <fabian.kasten@outlook.de>
2026-04-26 21:33:38 +0200

    Add transaction entity

--- src/main/java/com/banking/transaction/Transaction.java
+++ src/main/java/com/banking/transaction/Transaction.java
@@ -1,4 +1,37 @@
 package com.banking.transaction;
 
+import com.banking.account.Account;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn
+    @JsonIgnore
+    private Account account;
 }
---
+++ src/main/java/com/banking/transaction/TransactionRepository.java
@@ -0,0 +1,4 @@
+package com.banking.transaction;
+
+public class TransactionRepository {
+}
