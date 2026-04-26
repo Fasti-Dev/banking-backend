5ab109761773bf5835c43caaedc245cd543de8f2
Fasti-Dev <fabian.kasten@outlook.de>
2026-04-26 20:40:40 +0200

    Add account entity

---
+++ src/main/java/com/banking/account/Account.java
@@ -0,0 +1,32 @@
+package com.banking.account;
+
+import com.banking.customer.Customer;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String iban;
+
+    @NotNull
+    private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
