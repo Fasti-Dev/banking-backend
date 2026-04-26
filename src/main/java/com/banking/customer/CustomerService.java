7bb0e4fa1226654e6c82ab7493c65f9fe23a8de6
Fasti-Dev <fabian.kasten@outlook.de>
2026-04-26 20:14:35 +0200

    Add customer service

---
+++ src/main/java/com/banking/customer/CustomerService.java
@@ -0,0 +1,22 @@
+package com.banking.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
+}
