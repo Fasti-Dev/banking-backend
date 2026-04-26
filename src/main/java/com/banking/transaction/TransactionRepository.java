1910761fdfdbaabb3507d765068d77f0c5d1de6d
Fasti-Dev <fabian.kasten@outlook.de>
2026-04-26 21:36:45 +0200

    Add transaction repository

--- src/main/java/com/banking/transaction/TransactionRepository.java
+++ src/main/java/com/banking/transaction/TransactionRepository.java
@@ -1,4 +1,11 @@
 package com.banking.transaction;
 
-public class TransactionRepository {
+import com.banking.account.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByAccount(Account account);
 }
